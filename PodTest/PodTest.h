05760316b1cf30f64aa7932eea0f6ba210574acb
기술지원팀 이현석 <hyunseok82@sk.com>
2016-08-18 18:04:41 +0900

    Initial Commit

---
+++ PodTest/PodTest.h
@@ -0,0 +1,19 @@
+//
+//  PodTest.h
+//  PodTest
+//
+//  Created by 1100425 on 2016. 8. 18..
+//  Copyright © 2016년 SKTX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodTest.
+FOUNDATION_EXPORT double PodTestVersionNumber;
+
+//! Project version string for PodTest.
+FOUNDATION_EXPORT const unsigned char PodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodTest/PublicHeader.h>
+
+
